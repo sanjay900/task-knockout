81ec835a4d8b6c97d0499ad42d53877b63f9001c
Rafael Santos <formigarafa@gmail.com>
2016-11-30 16:39:00 +1300

    allow named arguments

--- lib/jira_integration/cli.rb
+++ lib/jira_integration/cli.rb
@@ -4,7 +4,7 @@ module JiraIntegration
       new(*args).run
     end
 
-    attr_accessor :raw_args, :command, :args
+    attr_accessor :raw_args, :command, :args, :named_args
 
     def initialize(*raw_args)
       self.raw_args = raw_args
@@ -13,6 +13,7 @@ module JiraIntegration
 
     def parse_arguments
       self.command = command_from_raw_args
+      self.named_args = named_args_from_raw_args
       self.args = args_from_raw_args
     end
 
@@ -28,11 +29,15 @@ module JiraIntegration
     end
 
     def args_from_raw_args
-      raw_args[1..-1]
+      raw_args[1..-1].grep(/^[^-]/)
+    end
+
+    def named_args_from_raw_args
+      Hash[raw_args[1..-1].grep(/^-/).map{|a| a.sub(/^--/, '').gsub(/-/, '_').split('=').tap{|i| i[0] = i[0].to_sym} }]
     end
 
     def run
-      Commands.send(command, *args)
+      Commands.send(command, *args, **named_args)
     end
   end
 end
