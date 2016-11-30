96d5955e63e42bbefa4221d0acf5aae673d741db
Rafael Santos <formigarafa@gmail.com>
2016-12-01 10:23:53 +1300

    add branches improve help

--- lib/jira_integration/commands.rb
+++ lib/jira_integration/commands.rb
@@ -1,20 +1,21 @@
 module JiraIntegration
   module Commands
-    Help.add(:Usage, "jira-cli <command> [options] [arguments]")
-    #   puts "filters                                                   print list of filters"
-    #   puts "issue <issue_id>                                          print issue"
-    #   puts "myself                                                    print current user information"
-    #   puts "show_filter <filter_id>                                   print details of a filter"
-    #   puts "lots to do yet"
-    # end
+    Help.add(:Usage, "jira-cli <command> [options] [arguments]\n")
 
+    JiraIntegration::Help.add(
+      :help,
+      "print help information",
+      "help [command]"
+    )
     def self.help(*args)
       command = args.first
       if ! command || command.empty?
-        command = :help
+        command = nil
+      else
+        command = command.to_sym
       end
 
-      puts JiraIntegration::Help.get(command.to_sym).join("\n")
+      puts JiraIntegration::Help.get(command).join("\n")
     end
 
     JiraIntegration::Help.add(
@@ -31,6 +32,17 @@ module JiraIntegration
     end
 
     JiraIntegration::Help.add(
+      :branches,
+      "list existing branches for issue",
+      "branches <issue_id>"
+    )
+    def self.branches(issue_id, *args)
+      data = JiraIntegration.api_client.issue(issue_id)
+      key = data[:key]
+      puts `git branch -a | grep -i '#{key}'`
+    end
+
+    JiraIntegration::Help.add(
       :filter,
       "print filtered issues",
       "filter <filter_id>"
@@ -42,21 +54,41 @@ module JiraIntegration
       puts data.to_yaml
     end
 
+    JiraIntegration::Help.add(
+      :filters,
+      "print current user existing filters",
+      "filters"
+    )
     def self.filters(*args)
       filters = JiraIntegration.api_client.my_filters
       data = filters.map{|f| {id: f[:id], name: f[:name], search_url: f[:searchUrl]}}
       puts data.to_yaml
     end
 
+    JiraIntegration::Help.add(
+      :issue,
+      "print inrmations about specified issue",
+      "issue <issue_id>"
+    )
     def self.issue(issue_id, *args)
       data = JiraIntegration.api_client.issue(issue_id)
       puts data.to_yaml
     end
 
+    JiraIntegration::Help.add(
+      :myself,
+      "print print information about current user",
+      "myself"
+    )
     def self.myself(*args)
       puts JiraIntegration.api_client.myself.to_yaml
     end
 
+    JiraIntegration::Help.add(
+      :show_filter,
+      "print information about the informed filter",
+      "filters"
+    )
     def self.show_filter(filter_id, *args)
       filter = JiraIntegration.api_client.filter(filter_id)
       puts filter.to_yaml
--- lib/jira_integration/help.rb
+++ lib/jira_integration/help.rb
@@ -8,11 +8,12 @@ module JiraIntegration
       entries[command.to_sym] = {summary: summary, description: description}
     end
 
-    def self.get(command = :help)
-      if command == :help
-        entries.map{|k, v| "#{k} #{v[:summary]}" }
-      else
+    def self.get(command)
+      if command && entries.has_key?(command)
         ["#{command} #{entries[command][:summary]}", entries[command][:description]]
+      else
+        max_length = entries.keys.map(&:size).max
+        entries.map{|k, v| "#{k.to_s.ljust(max_length + 3)} #{v[:summary]}" }
       end
     end
   end
