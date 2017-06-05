7e29f70d885d3526c28c432cce4e906cc1f53471
Rafael Santos <formigarafa@gmail.com>
2017-06-05 21:47:25 +1200

    remove jira only commands

--- lib/task_knockout/cli.rb
+++ lib/task_knockout/cli.rb
@@ -47,108 +47,11 @@ module TaskKnockout
       puts related_branches.join("\n")
     end
 
-    desc "filter <filter_id>", "print filtered issues"
-    def filter(filter_id)
-      search = TaskKnockout.api_client.search_by_filter(filter_id)
-      # data = search[:issues].map{|i| {id: i[:id], key: i[:key], summary: i[:fields][:summary], description: i[:fields][:description]} }
-      # data = search[:issues].map{|i| {id: i[:id], key: i[:key], summary: i[:fields][:summary]} }
-      # puts data.to_yaml
-      data = search[:issues].map do |i|
-        {
-          key: i[:key],
-          type: i[:fields][:issuetype][:name],
-          status: i[:fields][:status][:name],
-          summary: i[:fields][:summary],
-        }
-      end
-      tp data, :key, {type: {width: 20}}, {status: {width: 20}}, summary: {width: 130}
-    end
-
-    desc "filters", "print current user existing filters"
-    def filters
-      data = TaskKnockout.api_client.my_filters
-      # data = data.map{|f| {id: f[:id], name: f[:name], search_url: f[:searchUrl]}, jql: f[:jql]}}
-      # puts data.to_yaml
-      data = data.map{|f| {id: f[:id], name: f[:name]}}
-      tp data
-    end
-
-    desc "issue <issue_id>", "print information about specified issue"
-    def issue(issue_id)
-      issue = TaskKnockout.api_client.issue(issue_id,
-        fields: ['summary', 'description', 'issuetype', 'created', 'status', 'creator', 'reporter', 'transitions'],
-        expand: ['transitions'],
-      )
-      fields = issue[:fields]
-      transitions = issue[:transitions]
-      data = {
-        key: issue[:key],
-        summary: fields[:summary],
-        issuetype: fields[:issuetype][:name],
-        status: fields[:status][:name],
-        creator: fields[:creator][:displayName],
-        reporter: fields[:reporter][:displayName],
-        available_transitions: transitions.map{|t| t[:name] }
-      }
-      puts data.to_yaml
-      puts "description: #{fields[:description]}"
-    end
-
-    desc "issue_transitions <issue_id>", "list available transitions for specified issue"
-    def issue_transitions(issue_id)
-      data = TaskKnockout.api_client.issue_transitions(issue_id)
-      data = data[:transitions].map{|f| {id: f[:id], name: f[:name]} }
-      puts data.to_yaml
-    end
-
-    desc "myself", "print print information about current user"
-    def myself
-      puts TaskKnockout.api_client.myself.to_yaml
-    end
-
     desc "pull_request <issue_id> [branch_name]", "[NYI] create pull request for issue"
     def pull_request
       branch_name = 'feature/AD-1282-something-done'
       "git push --set-upstream origin '#{branch_name}'"
       "hub pull-request -m 'AD-1282: fix form preview' -b 'develop' -h '#{branch_name}'"
-    end
-
-    desc "show_filter", "print information about the informed filter"
-    def show_filter(filter_id)
-      filter = TaskKnockout.api_client.filter(filter_id)
-      data = {
-        id: filter[:id],
-        name: filter[:name],
-        owner: filter[:owner][:displayName],
-        jql: filter[:jql],
-        viewUrl: filter[:viewUrl],
-        # searchUrl: filter[:searchUrl],
-      }
-      puts data.to_yaml
-    end
-
-    desc "transition <issue_id> <transition id or name>", "transition a issue to another state"
-    def transition(issue_id, transition_id)
-      if transition_id.match /[a-zA-Z]/
-        state_name = commandify(transition_id)
-        response = TaskKnockout.api_client.issue_transitions(issue_id)
-        matching_transitions = response[:transitions].select{|t| commandify(t[:name]).include?(state_name) }
-        if matching_transitions.size == 0
-          puts "Could not find matching transition for issue."
-          puts "Available transitions:"
-          puts response[:transitions].map{|t| t[:name] }.to_yaml
-          return
-        elsif matching_transitions.size == 1
-          transition_id = matching_transitions.first[:id]
-        else
-          puts "multiple transitions matched, please be more specific"
-          puts "matched transitions:"
-          puts matching_transitions.map{|t| t[:name] }.to_yaml
-          return
-        end
-      end
-      data = TaskKnockout.api_client.transition(issue_id, transition_id)
-      puts data.to_yaml
     end
 
     private
